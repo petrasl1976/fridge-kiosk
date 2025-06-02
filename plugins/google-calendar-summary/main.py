72996eb639d152eb9045cd9f0fbdf498942eeee2
Petras Liumparas <petrasl@gmail.com>
2025-06-02 22:13:52 +0300

    fix

--- plugins/google-calendar-summary/main.py
+++ plugins/google-calendar-summary/main.py
@@ -490,7 +490,7 @@ def get_summary_events(config=None):
         return {'error': str(e)}
 
 def get_weather_now():
-    logger.info("!!! TEST: get_weather_now called !!!")
+    logger.info("!!! DEBUG: get_weather_now called !!!")
     try:
         logger.info("Fetching weather from http://localhost:8080/api/plugins/weather-forecast/data")
         resp = requests.get("http://localhost:8080/api/plugins/weather-forecast/data", timeout=10)
@@ -498,12 +498,8 @@ def get_weather_now():
         if resp.status_code == 200:
             data = resp.json()
             logger.info(f"Weather API data: {data}")
-            print("Weather API data:", data)
-            if "current" in data:
-                return data["current"]
-            else:
-                logger.error("No 'current' key in weather API response!")
-                return {}
+            print("Weather API data (DEBUG):", data)
+            return data  # Return full data for debug
         else:
             logger.error(f"Weather API returned status {resp.status_code}")
             return {}
